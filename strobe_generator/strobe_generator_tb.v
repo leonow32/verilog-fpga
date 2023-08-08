371c2d461565f4919ac1d9174723c9284d5878ea
leonow32 <leonow32@gmail.com>
2023-08-08 18:44:26 +0200

    Freezed

--- strobe_generator/strobe_generator_tb.v
+++ strobe_generator/strobe_generator_tb.v
@@ -43,13 +43,13 @@ module StrobeGenerator_tb();
 		$timeformat(-9, 3, "ns", 10);
 		$display("===== START =====");
 		$display("Clock");
-		$display(" - Frequency:          %f MHz", DUT.CLOCK_HZ / 1_000_000.0);
-		$display(" - Period:             %f ns", DUT.CLOCK_PERIOD_NS);
+		$display(" - Frequency: %f MHz", DUT.CLOCK_HZ / 1_000_000.0);
+		$display(" - Period: %f ns", DUT.CLOCK_PERIOD_NS);
 		$display("Strobe");
 		$display(" - Period requested: %d ns", DUT.PERIOD_NS);
-		$display(" - Period achieved:   %f ns", DUT.REAL_PERIOD_NS);
-		$display(" - Clock ticks:               %d", DUT.TICKS);
-		$display(" - Counter width:    %d", DUT.WIDTH);
+		$display(" - Period achieved: %f ns", DUT.REAL_PERIOD_NS);
+		$display(" - Clock ticks: %d", DUT.TICKS);
+		$display(" - Counter width: %d", DUT.WIDTH);
 		
 		
 		
