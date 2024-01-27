f27f22545fc908b1fa78bceb0d52d147f40ac24e
leonow32 <leonow32@gmail.com>
2024-01-27 11:44:37 +0100

    Optimized

--- double_dabble_combinational/double_dabble.v
+++ double_dabble_combinational/double_dabble.v
@@ -12,30 +12,30 @@ module DoubleDabble #(
 );
 	
 	// For loop iterators
-	integer i;
-	integer j;
+	integer bit;
+	integer digit;
 	
 	// Combinational logic
 	always @(*) begin
 		BCD_o = 0;
 		
 		// For each bit in the input
-		for(i=0; i<INPUT_BITS; i=i+1) begin
+		for(bit=0; bit<INPUT_BITS; bit=bit+1) begin
 			
 			// For each digit in the output
-			for(j=3; j<OUTPUT_BITS; j=j+4) begin
+			for(digit=0; digit<OUTPUT_BITS; digit=digit+4) begin
 				
 				// If a digit is >= 5
-				if(BCD_o[j-:4] >= 4'd5) begin
+				if(BCD_o[digit+:4] >= 4'd5) begin
 					
 					// Then add 3 to this digit
-					BCD_o[j-:4] = BCD_o[j-:4] + 4'd3;
+					BCD_o[digit+:4] = BCD_o[digit+:4] + 4'd3;
 				end
 			end
 			
 			// Shift output register
 			// and append another bit from the binary input
-			BCD_o = {BCD_o[OUTPUT_BITS-2:0], Binary_i[(INPUT_BITS-1)-i]};
+			BCD_o = {BCD_o[OUTPUT_BITS-2:0], Binary_i[(INPUT_BITS-1)-bit]};
 		end
 	end
 
